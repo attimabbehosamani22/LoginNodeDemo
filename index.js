5704ada5c8142dba581fbae763b415f65670b761
attimabbehosamani22 <attimabbe.hosamani@dxc.com>
2023-03-03 21:19:47 +0530

    3rd commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
+//const cors = require('cors')
 require('./models/config')
 const sData = require('./data.json')
 const userDetails = require('./routers/userroutes')
-app.use(cors())
+//app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
     res.send("sdata")
